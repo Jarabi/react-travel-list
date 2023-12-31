3974057b7d296624197cc67623c253cd7246b9b3
Alex Jarabi <jarabialex@gmail.com>
2024-01-01 00:10:07 +0300

    Controlled elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
-  { id: 2, description: 'Charger', quantity: 1, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
 ];
 
 export default function App() {
@@ -20,21 +22,40 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😝trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
